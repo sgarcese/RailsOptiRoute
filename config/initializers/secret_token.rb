f87dd9aa2f4005abd33416bf850b98d7c81db7cc
Santiago Garces <s.garces@sbenfocus.org>
2013-01-19 09:50:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RouteOptimization::Application.config.secret_token = '5386a2b4fd69ddbd44e390f37cd87c8128bbce5ec8a5794be77187aa6c8c50ef20c0de32cee2bd7f31efe9c93ee5f00d483725d7ff3e4f49c926f4b171776e5c'
